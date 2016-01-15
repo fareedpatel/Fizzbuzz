2d500c683471c03844e09f201e3f3f434aef6b40
fareedpatel <fareed_patel@hotmail.com>
2016-01-15 19:34:58 +0000

    second test fizzbuzz.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,9 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do 
  it 'returns "fizz" when we pass 3' do
- expect(fizzbuzz).to eq "fizz"
+ expect(fizzbuzz(3)).to eq "fizz"
  end
-
+ it 'returns "buzz" when we pass 5' do
+ expect(fizzbuzz(5)).to eq "buzz"
+ end
 end
